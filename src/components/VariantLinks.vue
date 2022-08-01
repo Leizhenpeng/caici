be041361dd134432663349ddbc1f32aab1d73589
RiverRay <laolei@forkway.cn>
2022-08-01 21:07:27 +0800

    chore: change content
    about author

--- src/components/VariantLinks.vue
+++ src/components/VariantLinks.vue
@@ -7,15 +7,23 @@ import { t } from '~/i18n'
     <p text-xl font-serif mb4>
       <b>{{ t('other-variants') }}</b>
     </p>
-    <ul flex="~ col gap-3" text-left w-50 m2 list-disc>
+    <ul flex="~ col gap-3" text-left w-60 m2 list-disc>
       <li>
-        <a href="https://pinyincaichengyu.com" target="_blank">拼音猜成语</a> - by <a href="https://twitter.com/_limboy">limboy</a>
+        <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank">Wordle</a> - by
+        <a href="https://www.nytimes.com/">The New York Times</a>
       </li>
       <li>
-        <a href="https://cheeaun.github.io/chengyu-wordle/" target="_blank">成语 Wordle</a> - by <a href="https://twitter.com/cheeaun">Chee Aun</a>
+        <a href="https://handle.antfu.me/" target="_blank">汉兜</a> - by <a href="https://twitter.com/antfu7">antfu</a>
       </li>
       <li>
-        <a href="https://apps.apple.com/cn/app/id1606194420" target="_blank">猜文字 (iOS)</a> - by <a href="https://twitter.com/OKJAKETO">Jinke Du</a>
+        <a href="https://pinyincaichengyu.com" target="_blank">拼音猜成语</a> - by <a
+          href="https://twitter.com/_limboy"
+        >limboy</a>
+      </li>
+      <li>
+        <a href="https://cheeaun.github.io/chengyu-wordle/" target="_blank">成语 Wordle</a> - by <a
+          href="https://twitter.com/cheeaun"
+        >Chee Aun</a>
       </li>
     </ul>
   </div>
@@ -25,6 +33,7 @@ import { t } from '~/i18n'
 .variant-links a {
   color: var(--c-primary);
 }
+
 .variant-links a:hover {
   color: var(--c-primary-deep);
 }
--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -88,13 +88,11 @@ const final = computed(() => ({ py: 'uo', zy: 'ㄨㄛ', sp: 'o' }[inputMode.valu
       {{ t('other-variants') }}
     </button>
     <div>
-      <span op40>inspired by </span><a href="https://www.powerlanguage.co.uk/wordle/" target="_blank" op50 hover:op80>Wordle</a>
+      <span op40>inspired by </span><a href="https://www.powerlanguage.co.uk/wordle/" target="_blank" op50 hover:op80>Handle</a>
       <span op40>, made by </span>
-      <a op50 hover:op80 href="https://twitter.com/antfu7" target="_blank">Anthony</a>
-      <span op40> & </span>
-      <a op50 hover:op80 href="https://twitter.com/iiiiiiines_____" target="_blank">Inès</a>
+      <a op50 hover:op80 href="https://github.com/Leizhenpeng" target="_blank">River</a>
     </div>
-    <a href="https://github.com/antfu/handle" target="_blank" flex="~ center gap-1" op50 hover:op80>
+    <a href="https://github.com/Leizhenpeng/caici" target="_blank" flex="~ center gap-1" op50 hover:op80>
       <div i-carbon-logo-github />
       Source Code
     </a>
