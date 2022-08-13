1312ba180d65408f2005cc960c9e4bd59fbf128e
RiverRay <laolei@forkway.cn>
2022-08-13 09:45:13 +0800

    chore: add hint.txt

--- src/components/Hint.vue
+++ src/components/Hint.vue
@@ -32,13 +32,13 @@ watch(showHint, (newVal) => {
       </button>
     </div>
     <p text-xl font-serif mb4>
-      <b>{{ t('hint') }}</b>
+      <b v-if="currentMeta.hintLevel === 0">{{ t('hint-ecourage') }}</b>
+      <b v-else>{{ t('hint') }}</b>
     </p>
     <div v-if="currentMeta.hintLevel === 0" flex="~ col gap-6 center" w-full>
       <!-- <div>{{ t('hint-tip') }}</div> -->
-      <div :key="hintTipKey">
-        {{ getRandomHint() }}
-      </div>
+      <!-- new line for hint tip -->
+      <div :key="hintTipKey" v-html="getRandomHint().replace(/NL/g, ' <br> ')" />
 
       <div flex="~ row between space-x-1" w-50>
         <button class="btn bg-mis" @click="close">
@@ -50,9 +50,14 @@ watch(showHint, (newVal) => {
       </div>
     </div>
     <div v-else flex="~ col center gap-4">
-      <div>{{ t('hint-note') }} <b>{{ currentMeta.hintLevel === 2 ? t('hanzi') : t('ziyin') }}</b></div>
+      <div>
+        {{ t('hint-note') }} <b>{{ currentMeta.hintLevel === 2 ? t('hanzi') : t('ziyin') }}</b>
+      </div>
       <CharBlock :char="currentMeta.hintLevel === 2 ? parsed : masked" />
-      <button v-if="currentMeta.hintLevel === 1" class="btn bg-mis" @click="currentMeta.hintLevel = 2">
+      <button
+        v-if="currentMeta.hintLevel === 1" class="btn bg-mis"
+        @click="currentMeta.hintLevel = 2"
+      >
         {{ t('more-hint') }}
       </button>
       <button v-if="currentMeta.hintLevel === 2" class="btn bg-ok" @click="close">
