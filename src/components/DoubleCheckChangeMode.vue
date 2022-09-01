2cd772e7dd9027c528fa1c9148597b30f3383570
RiverRay <laolei@forkway.cn>
2022-09-01 20:41:53 +0800

    chore: add doubleCheckExit

--- src/components/DoubleCheckChangeMode.vue
+++ src/components/DoubleCheckChangeMode.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-import { showFailed } from '~/state'
-import { currentMeta } from '~/storage'
+import { useRouter } from 'vue-router'
 import { t } from '~/i18n'
+import { showDoubleCheckExit } from '~/state'
+const router = useRouter()
 
-function revealAnswer() {
-  showFailed.value = false
-  currentMeta.value.answer = true
-  currentMeta.value.failed = true
+const close = () => {
+  showDoubleCheckExit.value = false
+}
+
+function goSolo() {
+  close()
+  router.replace({ name: 'solo' })
 }
 </script>
 
 <template>
   <div p5 flex="~ col center">
-    <div i-carbon-face-dizzy text-5xl op50 m4 />
-    <div>
-      {{ t('failed-1') }}<br><br>
-      {{ t('failed-2') }}<br>
-      {{ t('failed-3') }}
+    <div i-mdi-emoticon-wink-outline text-5xl op50 m4 />
+    <div text-sm>
+      朋友们托我向你道别~<br>
     </div>
-    <div p4 flex="~ gap-5 center">
-      <button btn @click="showFailed = false">
+    <div py6 flex="~ gap-5 center">
+      <button btn @click="close">
         {{ t('continue') }}
       </button>
-      <button icon-btn flex="inline gap-1 center" text="base mis!" @click="revealAnswer()">
+      <button icon-btn flex="inline gap-1 center" text="base mis!" @click="goSolo">
         <div i-mdi-emoticon-confused-outline /> {{ t('check_exit') }}
       </button>
     </div>
